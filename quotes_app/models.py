e675cf05c748b3071f5918093a8a205384a55286
Михаил <koptseff.mikhail@yandex.ru>
2022-04-12 10:42:15 +0300

    quotes_app

---
+++ quotes_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Quote(models.Model):
+    title = models.CharField(max_length=50)
+
+    likes = models.PositiveIntegerField(default=0)
+
+    def __dir__(self):
+        return self.title
